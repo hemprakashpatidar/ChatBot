4996a488a88d7da587678a9e5e44fe1055a179c4
hemprakashpatidar <57744657+hemprakashpatidar@users.noreply.github.com>
2021-02-23 00:22:11 +0530

    Add files via upload

---
+++ build/generated/src/org/apache/jsp/FAQ_jsp.java
@@ -0,0 +1,77 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class FAQ_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.Vector _jspx_dependants;
+
+  private org.apache.jasper.runtime.ResourceInjector _jspx_resourceInjector;
+
+  public Object getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.apache.jasper.runtime.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"\n");
+      out.write("   \"http://www.w3.org/TR/html4/loose.dtd\">\n");
+      out.write("\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <title>JSP Page</title>\n");
+      out.write("    </head>\n");
+      out.write("    <body>\n");
+      out.write("        <center>  <h1>Frequently Asked questions</h1></center>\n");
+      out.write("        <br>        Q- What is the rank of college?\n");
+      out.write("   <br>     A- It ranked 2nd in MP\n");
+      out.write("       <br> Q- What is Examination pattern?\n");
+      out.write("        <br>A- Exam is based on CBCS system. There are 3 mid-Sem exams conducted in every semester which comprises of 20 marks each. Best of Two mid-sem marks are taken \\n and added to end semester exam comprising of 60 Marks resulting in 100 marks total.\n");
+      out.write("        <br>Q- What is college timing?\n");
+      out.write("        <br>A- It is generally from 10 A.M. to 4 P.M. but it varies for students studying in different years as per their time table\n");
+      out.write("    </body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
